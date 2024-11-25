d1e2e47acb9ab9c53ec5a12807f6ff7d1c2a5581
PyWebSol <safephone1.12@gmail.com>
2024-11-25 14:12:24 +0300

    Updated mappings

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
 -- Configuration documentation can be found with `:h astrocore`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
@@ -67,6 +65,7 @@ return {
         -- setting a mapping to false will disable it
         -- ["<C-S>"] = false,
         [";"] = { ":", desc = "Enter command mode" },
+        ["ж"] = { ":", desc = "Enter command mode (RU)" },
         ["<C-b>"] = { "<cmd>Neotree toggle<cr>", desc = "Toggle Explorer" },
       },
       i = {
